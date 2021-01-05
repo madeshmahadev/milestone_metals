296ba493a94940359df53e22302910e27b2adda4
madeshmahadev <madeshmaddy9@gmail.com>
2021-01-06 03:50:49 +0530

    changed to uikit

---
+++ assets/js/main.js
@@ -0,0 +1,146 @@
+(function ($) {
+	$(document).ready(function() {
+		   preloaderCheck();
+	});	
+
+
+		function showPreloader() {
+		  $("#preloader").fadeIn("slow");
+			console.log('offcanvas beforeshow 1');
+		}
+
+		function hidePreloader() {
+		  $("#preloader").delay(2000).fadeOut("slow");
+		  console.log('offcanvas beforeshow 2');
+		}
+
+		function preloaderCheck() {
+			showPreloader();
+			$(window).load(function() {
+				hidePreloader();
+				console.log('offcanvas beforeshow 3');
+			});
+		}
+
+})(jQuery);
+
+
+UIkit.util.on('#offcanvas-nav-primary', 'beforeshow', function () {
+  // do something
+  console.log('offcanvas beforeshow');
+});
+
+UIkit.util.on('#offcanvas-nav-primary', 'show', function () {
+  // do something
+  console.log('offcanvas show');
+});
+
+UIkit.util.on('#offcanvas-nav-primary', 'shown', function () {
+  // do something
+  console.log('offcanvas shown');
+});
+
+UIkit.util.on('#offcanvas-nav-primary', 'beforehide', function () {
+  // do something
+  console.log('offcanvas beforehide');
+});
+
+UIkit.util.on('#offcanvas-nav-primary', 'hide', function () {
+  // do something
+  console.log('offcanvas hide');
+});
+
+UIkit.util.on('#offcanvas-nav-primary', 'hidden', function () {
+  // do something
+  console.log('offcanvas hidden');
+});
+
+
+function fitElementToParent(el, padding) {
+  var timeout = null;
+  function resize() {
+    if (timeout) clearTimeout(timeout);
+    anime.set(el, {scale: 1});
+    var pad = padding || 0;
+    var parentEl = el.parentNode;
+    var elOffsetWidth = el.offsetWidth - pad;
+    var parentOffsetWidth = parentEl.offsetWidth;
+    var ratio = parentOffsetWidth / elOffsetWidth;
+    timeout = setTimeout(anime.set(el, {scale: ratio}), 10);
+  }
+  resize();
+  window.addEventListener('resize', resize);
+}
+
+var sphereAnimation = (function() {
+
+  var sphereEl = document.querySelector('.sphere-animation');
+  var spherePathEls = sphereEl.querySelectorAll('.sphere path');
+  var pathLength = spherePathEls.length;
+  var hasStarted = false;
+  var aimations = [];
+
+  fitElementToParent(sphereEl);
+
+  var breathAnimation = anime({
+    begin: function() {
+      for (var i = 0; i < pathLength; i++) {
+        aimations.push(anime({
+          targets: spherePathEls[i],
+          stroke: {value: ['rgba(255, 255, 255,1)', 'rgba(176, 174, 174,.35)'], duration: 500},
+          translateX: [2, -4],
+          translateY: [2, -4],
+          easing: 'easeOutQuad',
+          autoplay: false
+        }));
+      }
+    },
+    update: function(ins) {
+      aimations.forEach(function(animation, i) {
+        var percent = (1 - Math.sin((i * .35) + (.0022 * ins.currentTime))) / 2;
+        animation.seek(animation.duration * percent);
+      });
+    },
+    duration: Infinity,
+    autoplay: false
+  });
+
+  var introAnimation = anime.timeline({
+    autoplay: false
+  })
+  .add({
+    targets: spherePathEls,
+    strokeDashoffset: {
+      value: [anime.setDashoffset, 0],
+      duration: 3900,
+      easing: 'easeInOutCirc',
+      delay: anime.stagger(190, {direction: 'reverse'})
+    },
+    duration: 2000,
+    delay: anime.stagger(60, {direction: 'reverse'}),
+    easing: 'linear'
+  }, 0);
+
+  var shadowAnimation = anime({
+      targets: '#sphereGradient',
+      x1: '25%',
+      x2: '25%',
+      y1: '0%',
+      y2: '75%',
+      duration: 30000,
+      easing: 'easeOutQuint',
+      autoplay: false
+    }, 0);
+
+  function init() {
+    introAnimation.play();
+    breathAnimation.play();
+    shadowAnimation.play();
+  }
+  
+  init();
+
+})();
+
+
+
